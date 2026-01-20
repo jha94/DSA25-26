5c95848c9d6d87936ea66d6749dacc743807f21e
Mr. Jha <prashantjha9549@gmail.com>
2026-01-20 20:12:21 +0530

    master :- DSA done for 20/01/2026

--- mostWater.js
+++ mostWater.js
@@ -1,16 +1,19 @@
 const maxArea = (heights) => {
     let left = 0;
-    let right = heights.length - 1
+    let right = heights.length - 1;
     let max = 0;
-    for (let index = 0; index < heights.length; index++) {
-        max = Math.max(max, (Math.min(heights[left], heights[right]) * (right - left)))
+    while (left < right) {
+        const width = right - left;
+        const height = Math.min(heights[left], heights[right]);
+        max = Math.max(max, width * height);
         if (heights[left] < heights[right]) {
-            left++
+            left++;
         } else {
-            right--
+            right--;
         }
     }
-    return max
-}
+    return max;
+};
 
-console.log(maxArea([, , ]))
+
+console.log(maxArea([, ,]))
