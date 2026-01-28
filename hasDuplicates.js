b0c16b02813d15605cc438879bd9da74ce967160
Mr. Jha <prashantjha9549@gmail.com>
2026-01-28 09:37:36 +0530

    master:- 28/01/2026

---
+++ hasDuplicates.js
@@ -0,0 +1,12 @@
+const hasDuplicate = (nums) => {
+    const set = new Set();
+    for (let num of nums) {
+        if (set.has(num)) {
+            return true
+        }
+        set.add(num)
+    }
+    return false
+}
+
+console.log(hasDuplicate([1, 2, 3, 3]))
---
+++ isAnagram.js
@@ -0,0 +1,17 @@
+const isAnagram = (s,t)=>{
+    if(s.length!==t.length) return false
+    const map = new Map();
+    for(let char of s){
+        map.set(char, (map.get(char)||0)+1)
+    }
+    for(let char of t){
+        if(!map.has(char)) return false
+        map.set(char, map.get(char)-1)
+        if(map.get(char)===0){
+            map.delete(char)
+        }
+    }
+    return map.size===0
+}
+
+console.log(isAnagram('jar', 'jam'))
