c2023caa6d6d1ea0ec1f1e73b9f67d83c3658dcf
Prashant Jha <prashantjha@Prashants-MacBook-Air.local>
2026-02-07 19:58:50 +0530

    07/02/2026

--- minInRotated.js
+++ minInRotated.js
@@ -1,27 +1,15 @@
 const minInRotated = (nums) => {
-    let min = Infinity
     let left = 0;
     let right = nums.length-1
-    while(left<=right){
+    while(left<right){
         let mid = Math.floor((left+right)/2)
-        if(nums[mid]<=min){
-            min = nums[mid]
-        } 
-        if(nums[left]<=nums[mid]){
-            if(min<nums[mid]&&min>=nums[left]){
-                right = mid-1
-            } else{
-                left = mid+1
-            }
+        if(nums[mid]>nums[right]){
+            left = mid+1
         } else{
-            if(min>nums[mid] && min<=nums[right]){
-                left = mid+1
-            } else{
-                right = mid-1
-            }
+            right = mid
         }
     }
-    return min
+    return nums[left]
 }
 
-console.log(minInRotated([4,5,6,7]))
+console.log(minInRotated([3,4,5,6,1,2]))
