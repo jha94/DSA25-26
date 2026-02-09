3b96ef8210ef7c84aa95861bdf6c3eb4d8a7d70b
Prashant Jha <prashantjha@Prashants-MacBook-Air.local>
2026-02-09 18:58:45 +0530

    09/02/2026

--- missingNumber.js
+++ missingNumber.js
@@ -1,10 +1,9 @@
 const missingNumber = (nums) => {
-    for(let index=0;index<nums.length;index++){
-        if(!nums.includes(index)){
+    for (let index = 0; index < nums.length; index++) {
+        if (!nums.includes(index)) {
             return index
         }
     }
     return nums.length
 }
-
-console.log(missingNumber([0,1]))
+console.log(missingNumber([0, 1]))
