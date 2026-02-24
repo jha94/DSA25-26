77ea0976b1bf14b6a69e5245a43dee26b570bea6
Prashant Jha <prashantjha9549@gmail.com>
2026-02-24 11:11:15 +0530

    master:- Binary tree

--- binaryTree.js
+++ binaryTree.js
@@ -1,12 +1,12 @@
 class Node {
     constructor(val) {
-        this.val = val
+        this.val = val;
         this.left = null
         this.right = null
     }
 }
 
-class BinaryTree {
+class BianryTree {
     constructor() {
         this.root = null
     }
@@ -14,13 +14,12 @@ class BinaryTree {
         const node = new Node(val)
         if (this.root === null) {
             this.root = node
-            return
         } else {
             this.insertNode(this.root, node)
         }
     }
     insertNode(root, node) {
-        const queue = [root]
+        let queue = [root]
         while (queue.length) {
             const currentNode = queue.shift()
             if (currentNode.left === null) {
@@ -35,25 +34,70 @@ class BinaryTree {
             }
         }
     }
-    levelOrder(root) {
-        if (!root) return null
-        const queue = [root]
-        while (queue.length) {
-            const currentNode = queue.shift()
-            if (currentNode) {
-                console.log(currentNode.val);
-                queue.push(currentNode.left)
-                queue.push(currentNode.right)
+    
+    levelorder(root){
+        let queue = [root]
+        while(queue.length){
+            const currentHead = queue.shift()
+            if(currentHead){
+            console.log(currentHead.val)
+            queue.push(currentHead.left)
+            queue.push(currentHead.right)
             }
+        }
+    }
+
+    search(root, val){
+        const queue = [root]
+        while(queue.length){
+            const currentHead = queue.shift()
+            if(currentHead){
+                if(currentHead.val===val){
+                    return val
+                }
+                queue.push(currentHead.left)
+                queue.push(currentHead.right)
+            }
+        }
+        return 'Oops!! value not found.'
+    }
+
+    // root, left, right [10, 20, 40, 50, 30]
+    preorder(root){
+        if(root){
+            console.log(root.val);
+            this.preorder(root.left)
+            this.preorder(root.right)
+        }
+    }
+
+    // left, root, right [40, 20, 50, 10, 30]
+    inorder(root){
+        if(root){
+            this.inorder(root.left)
+            console.log(root.val);
+            this.inorder(root.right)
+        }
+    }
+
+    // left, right, root [40, 50, 20, 30, 10]
+    postorder(root){
+        if(root){
+            this.postorder(root.left)
+            this.postorder(root.right)
+            console.log(root.val);
         }
     }
 }
 
-const bt = new BinaryTree()
+const bt = new BianryTree()
 bt.insert(10)
 bt.insert(20)
 bt.insert(30)
 bt.insert(40)
 bt.insert(50)
-bt.levelOrder(bt.root)
-
+// bt.levelorder(bt.root)
+// bt.preorder(bt.root)
+// bt.inorder(bt.root)
+bt.postorder(bt.root)
+// console.log(bt.search(bt.root, 500))
