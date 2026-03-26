1dbfe3b57cae5a302d7bd4e7b04c4fa222a6b866
Prashant Jha <prashantjha9549@gmail.com>
2026-03-26 12:26:52 +0530

    master:- backtracking for 26/02/2026

--- backtracking.js
+++ backtracking.js
@@ -1,99 +1,37 @@
-const isPowerOfTwo = (number) => {
-  const check = (num, divider) => {
-    if (num % divider === 0) return true;
-    if (num % divider === 1 || num % divider < 0) return false;
-    return check(num, divider * 2);
-  };
-  return check(number, 2);
-};
 // console.log(isPowerOfTwo(7));
-
-const fib = (num) => {
-  if (num === 0) return 0;
-  if (num === 1) return 1;
-  return fib(num - 1) + fib(num - 2);
-};
 // console.log(fib(3));
-
-const permutation = (nums) => {
-  const res = [];
-  const getPermutations = (curr, index) => {
-    if (index === nums.length) {
-      res.push([...curr]);
-      return;
-    }
-    for (let ind = index; ind < nums.length; ind++) {
-      [nums[index], nums[ind]] = [nums[ind], nums[index]];
-      getPermutations(nums, ind + 1);
-      [nums[index], nums[ind]] = [nums[ind], nums[index]];
-    }
-  };
-  getPermutations([], 0);
-  return res;
-};
 // console.log(permutation([1, 2]));
-
-const subsets = (nums) => {
-  const res = [];
-  const getSubsets = (curr, index) => {
-    for (let ind = index; ind < nums.length; ind++) {
-      curr.push(nums[ind]);
-      res.push([...curr]);
-      getSubsets(curr, ind + 1);
-      curr.pop();
-    }
-  };
-  getSubsets([], 0);
-  const hash = {};
-  for (let el of res) {
-    if (hash[el]) continue;
-    hash[el] = el;
-  }
-  return Object.values(hash);
-};
 // console.log(subsets([1, 2, 2]));
+// console.log(wordPermutation("CODE"));
+// console.log(combinationSum([1, 2, 2], 3));
 
-const wordPermutation = (word) => {
+const partition = (string) => {
   const res = [];
-  const getPermutation = (curr, visited) => {
-    if (curr.length === word.length) {
-      res.push(curr.join(""));
+
+  const isPolindrome = (str, start, end) => {
+    while (start < end) {
+      if (str[start] !== str[end]) return false;
+      start++;
+      end--;
+    }
+    return true;
+  };
+
+  const backtrack = (curr, index) => {
+    if (index === string.length) {
+      res.push([...curr]);
       return;
     }
-    for (let index = 0; index < word.length; index++) {
-      if (visited.has(index)) continue;
-      curr.push(word[index]);
-      visited.add(index);
-      getPermutation(curr, visited);
-      curr.pop();
-      visited.delete(index);
+    for (let ind = index; ind < string.length; ind++) {
+      if (isPolindrome(string, index, ind)) {
+        curr.push(string.substring(index, ind + 1));
+        backtrack(curr, ind + 1);
+        curr.pop();
+      }
     }
   };
-  getPermutation([], new Set());
+  backtrack([], 0);
   return res;
 };
-// console.log(wordPermutation("CODE"));
 
-const targetSum = (nums, target) => {
-  const res = [];
-  const getNums = (arr, currentSum, index) => {
-    if (currentSum < 0) return;
-    if (currentSum === 0) {
-      res.push([...arr]);
-      return;
-    }
-    for (let ind = index; ind < nums.length; ind++) {
-      arr.push(nums[ind]);
-      getNums(arr, currentSum - nums[ind], ind);
-      arr.pop();
-    }
-  };
-  getNums([], target, 0);
-  const hash = {};
-  for (let val of res) {
-    if (hash[val]) continue;
-    hash[val] = val;
-  }
-  return Object.values(hash);
-};
-console.log(targetSum([1, 2, 2], 3));
+console.log(partition("aab"));
