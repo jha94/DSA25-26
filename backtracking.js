05369d252254d11de44fe5833677f087ad7f32b5
Prashant Jha <prashantjha9549@gmail.com>
2026-03-30 12:21:28 +0530

    master :- backtracking revised

--- backtracking.js
+++ backtracking.js
@@ -33,5 +33,65 @@ const partition = (string) => {
   backtrack([], 0);
   return res;
 };
+// console.log(partition("aab"));
 
-console.log(partition("aab"));
+const combinations = (range, size) => {
+  const res = [];
+  let mainArr = [];
+  for (let index = 1; index <= range; index++) {
+    mainArr.push(index);
+  }
+  const getCombinations = (arr, start) => {
+    if (arr.length === size) {
+      res.push([...arr]);
+      return;
+    }
+    for (let ind = start; ind < range; ind++) {
+      arr.push(mainArr[ind]);
+      getCombinations(arr, ind + 1);
+      arr.pop();
+    }
+  };
+  getCombinations([], 0);
+  return res;
+};
+// console.log(combinations(3, 2));
+
+// const wordPermutation = (str) => {
+//   const res = []
+//   const getWordPermutations = (string, index) => {
+//     if(string.length===index){
+//       res.push(string.join(''))
+//       return
+//     }
+//     for(let ind=index;ind<string.length;ind++){
+//       [string[index], string[ind]] = [string[ind], string[index]]
+//       getWordPermutations(string, ind+1);
+//       [string[index], string[ind]] = [string[ind], string[index]]
+//     }
+//   }
+//   getWordPermutations(str.split(''), 0)
+//   return res
+// }
+// console.log(wordPermutation("CODE"));
+
+const combinationSum = (nums, target) => {
+  const res = [];
+  const getCombinations = (curr, targetSum, start) => {
+    if (targetSum < 0) return;
+    if (targetSum === 0) {
+      res.push([...curr]);
+      return;
+    }
+    for (let index = start; index < nums.length; index++) {
+      curr.push(nums[index]);
+      getCombinations(curr, targetSum - nums[index], index);
+      curr.pop();
+    }
+  };
+  getCombinations([], target, 0);
+  return res;
+};
+console.log(combinationSum([1, 2, 2], 3));
+
+console.log(combinationSum([3, 4, 5], 16));
