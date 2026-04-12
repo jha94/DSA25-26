5d16f73989443e9bb60da44dc1cd470b14ccf6d8
Prashant Jha <prashantjha9549@gmail.com>
2026-04-12 23:12:54 +0530

    master:- graph DFS done

--- graph.js
+++ graph.js
@@ -8,46 +8,62 @@
 // }
 // buildAdjMatrix([[0,1], [2, 3], [0,4]], 5)
 
-// const buildAdjList = (edges) => {
-//   const adjList = {}
-//   for(let edge of edges){
+// const buildAdjList = (adj) => {
+//   const visited = {}
+//   for(let edge of adj){
 //     const [src, dest] = edge
-//     if(adjList[src]){
-//       adjList[src].push(dest)
+//     if(visited[src]){
+//       visited[src].push(dest)
 //     } else{
-//       adjList[src] = [dest]
+//       visited[src] = [dest]
 //     }
 //   }
-//   console.log(adjList);
-
+//   return visited
 // }
-// buildAdjList([[0,1], [2, 3], [0,4]])
+// console.log(buildAdjList([[0,1], [2, 3], [0,4]]))
 
-const graphBfs = (adj) => {
-  const visited = {};
-  const res = [];
-  const queue = [];
-  for (let index in adj) {
-    if (!visited[index]) {
-      bfs(index);
-    }
-  }
-  function bfs(index) {
-    visited[index] = true;
-    res.push(index);
-    queue.push(index);
-    while (queue.length) {
-      const current = queue.shift();
-      for (let neighbour of adj[current]) {
-        if (!visited[neighbour]) {
-          visited[neighbour] = true;
-          res.push(neighbour);
-          queue.push(neighbour);
-        }
+// const graphBfs = (adj) => {
+//   const visited = {};
+//   const res = [];
+//   const queue = [];
+//   for (let index in adj) {
+//     if (!visited[index]) {
+//       bfs(index);
+//     }
+//   }
+//   function bfs(index) {
+//     visited[index] = true;
+//     res.push(index);
+//     queue.push(index);
+//     while (queue.length) {
+//       const current = queue.shift();
+//       for (let neighbour of adj[current]) {
+//         if (!visited[neighbour]) {
+//           visited[neighbour] = true;
+//           res.push(neighbour);
+//           queue.push(neighbour);
+//         }
+//       }
+//     }
+//   }
+//   return res;
+// };
+
+// console.log(graphBfs([[2, 3, 1], [0], [0, 4], [0], [4]]));
+
+const graphDfs = (adj) => {
+  const answer = []
+  const visited = {}
+  const dfs = (index) => {
+    visited[index] = true
+    answer.push(index)
+    for(let neighbour of adj[index]){
+      if(!visited[neighbour]){
+        dfs(neighbour)
       }
     }
   }
-  return res;
-};
-
-console.log(graphBfs([[2, 3, 1], [0], [0, 4], [0], [4]]));
+  dfs(0)
+  return answer
+}
+console.log(graphDfs([[2, 3, 1], [0], [0, 4], [0], [2]]));
