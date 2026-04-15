79f184dee0040b8d383e199d367958af8c285403
Prashant Jha <prashantjha9549@gmail.com>
2026-04-15 12:27:34 +0530

    graph :- find judges fixed

--- graph.js
+++ graph.js
@@ -1,24 +1,14 @@
-// const buildAdjMatrix = (edges, numOfNodes) =>{
-//   const adjMat = Array.from({length:numOfNodes}, ()=>[])
-//   for(let edge of edges){
-//     const [src, dest] = edge
-//     adjMat[src][dest] = true
-//   }
-//   console.log(adjMat);
-// }
-// buildAdjMatrix([[0,1], [2, 3], [0,4]], 5)
-
-// const buildAdjList = (adj) => {
-//   const visited = {}
-//   for(let edge of adj){
-//     const [src, dest] = edge
-//     if(visited[src]){
-//       visited[src].push(dest)
+// const buildAdjList = (list) => {
+//   const adjList = {}
+//   list.map((value)=>{
+//     const [src, dest] = value
+//     if(adjList[src]){
+//       adjList[src].push(dest)
 //     } else{
-//       visited[src] = [dest]
+//       adjList[src] = [dest]
 //     }
-//   }
-//   return visited
+//   })
+//   return adjList
 // }
 // console.log(buildAdjList([[0,1], [2, 3], [0,4]]))
 
@@ -31,6 +21,7 @@
 //       bfs(index);
 //     }
 //   }
+
 //   function bfs(index) {
 //     visited[index] = true;
 //     res.push(index);
@@ -51,19 +42,48 @@
 
 // console.log(graphBfs([[2, 3, 1], [0], [0, 4], [0], [4]]));
 
-const graphDfs = (adj) => {
-  const answer = []
-  const visited = {}
-  const dfs = (index) => {
-    visited[index] = true
-    answer.push(index)
-    for(let neighbour of adj[index]){
-      if(!visited[neighbour]){
-        dfs(neighbour)
-      }
+// const graphDfs = (list) => {
+//   const answer = [];
+//   const visited = {}
+//   const dfs = (index) => {
+//     answer.push(index)
+//     visited[index]=true
+//     for(let neighbour of list[index]){
+//       if(!visited[neighbour]){
+//         dfs(neighbour)
+//       }
+//     }
+//   }
+//   dfs(0)
+//   return answer
+// }
+// console.log(graphDfs([[2, 3, 1], [0], [0, 4], [0], [2]]));
+
+const findJudge = (n, trust) => {
+  const incoming = {};
+  const outgoing = {};
+  for (let index = 1; index <= n; index++) {
+    incoming[index] = 0;
+    outgoing[index] = 0;
+  }
+  for (let value of trust) {
+    const [src, dest] = value;
+    outgoing[src] += 1;
+    incoming[dest] += 1;
+  }
+  for (let [key, value] of Object.entries(incoming)) {
+    if (value === n - 1 && outgoing[key] === 0) {
+      return key;
     }
   }
-  dfs(0)
-  return answer
-}
-console.log(graphDfs([[2, 3, 1], [0], [0, 4], [0], [2]]));
+  return -1;
+};
+
+const judge = findJudge(3, [
+  [1, 3],
+  [2, 3],
+  [3, 1],
+  [3, 2],
+]);
+
+console.log(judge);
