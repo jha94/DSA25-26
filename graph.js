6b5ca88ac8fd1e6f0da68f801823d3a06353038a
Prashant Jha <prashantjha9549@gmail.com>
2026-04-20 13:04:46 +0530

    master :- graph revise

--- graph.js
+++ graph.js
@@ -201,3 +201,35 @@ const maxArea = maxAreaOfIsland([
   [0, 1, 0, 0, 1],
 ]);
 // console.log(maxArea);
+
+const closedIsland = (grid) => {
+  let count = 0;
+  const dfs = (i, j) => {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+      return 0;
+    }
+    if (grid[i][j] === 1) {
+      return 1;
+    }
+    grid[i][j] = 1;
+    const up = dfs(i + 1, j);
+    const down = dfs(i - 1, j);
+    const left = dfs(i, j + 1);
+    const right = dfs(i, j - 1);
+    return up && down && left && right;
+  };
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 0) {
+        count += dfs(i, j);
+      }
+    }
+  }
+  return count || -1;
+};
+const numOfClosedIsland = closedIsland([
+  [0, 0, 1, 0, 0],
+  [0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0],
+]);
+console.log("numOfClosedIsland", numOfClosedIsland);
